a79bc971ded05e6a4ad73aa79db361e8043d903f
JulyKikuAkita <b92701105@gmail.com>
2021-10-06 23:31:16 -0700

    use User class: ObservableObject

--- iExpense/ContentView.swift
+++ iExpense/ContentView.swift
@@ -4,19 +4,24 @@
 //
 //  Created by Ifang Lee on 10/5/21.
 //
+// Use the object for multiple views
+// Steps:
+// 1. Make a class that conforms to the ObservableObject protocol.
+// 2. Mark some properties with @Published so that any views using the class get updated when they change.
+// 3. Create an instance of our class using the @ObservedObject property wrapper.
 
 import SwiftUI
 
-struct User {
-    var firstName = "Noodles"
-    var lastName = "Lee"
+class User: ObservableObject {
+    @Published var firstName = "Noodles"
+    @Published var lastName = "Lee"
 }
 
 struct ContentView: View {
-    @State private var user = User()
+    @ObservedObject var user = User()
 
     var body: some View {
-        VStack{
+        VStack(alignment: .center){
             Text("Your name is \(user.firstName) \(user.lastName).")
 
             TextField("First name", text: $user.firstName)
