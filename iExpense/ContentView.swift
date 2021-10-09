00c7577e27fdad6eda019b3826d3f215c3016d99
JulyKikuAkita <b92701105@gmail.com>
2021-10-09 14:21:47 -0700

    add list of ExpenseItem

--- iExpense/ContentView.swift
+++ iExpense/ContentView.swift
@@ -7,31 +7,42 @@
 
 import SwiftUI
 
-struct User: Codable {
-    var firstName: String
-    var lastName: String
+struct ExpenseItem: Codable {
+    let name: String
+    let type: String
+    let amount: Int
+}
+
+class Expenses: ObservableObject {
+    @Published var items = [ExpenseItem]()
 }
 
 struct ContentView: View {
-    @State private var tapCount = UserDefaults.standard.integer(forKey: "Tap")
-    @State private var user = UserDefaults.standard.object(forKey: "UserData") as? User ?? User(firstName: "Noodles", lastName: "Lee")
+    @ObservedObject var expenses = Expenses()
 
     var body: some View {
-        VStack {
-            Button("Tap count: \(tapCount)") {
-                tapCount += 1
-                UserDefaults.standard.set(tapCount, forKey: "Tap")
-            }
-
-            Button("Save User: \(user.firstName) \(user.lastName)") {
-                let encoder = JSONEncoder()
-                if let data = try? encoder.encode(user) {
-                    UserDefaults.standard.set(data, forKey: "UserData")
+        NavigationView {
+            List {
+                ForEach(expenses.items, id: \.name) { item in
+                    Text(item.name)
                 }
+                .onDelete(perform: removeItems)
             }
+            .navigationTitle("iExpense")
+            .navigationBarItems(trailing:
+                Button(action: {
+                    let expense = ExpenseItem(name: "Test", type: "A", amount: 10)
+                    expenses.items.append(expense)
+                }) {
+                    Image(systemName: "plus")
+                }
+            )
         }
     }
 
+    func removeItems(at offsets: IndexSet) {
+        expenses.items.remove(atOffsets: offsets)
+    }
 }
 
 
