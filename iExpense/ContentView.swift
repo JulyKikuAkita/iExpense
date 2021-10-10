89897405d33e2af3333118c8e0cf11076a64fc42
JulyKikuAkita <b92701105@gmail.com>
2021-10-09 19:35:59 -0700

    add Add expense view and presentationMode environment property wrapper

--- iExpense/ContentView.swift
+++ iExpense/ContentView.swift
@@ -7,36 +7,68 @@
 
 import SwiftUI
 
-struct ExpenseItem: Codable {
+struct ExpenseItem: Identifiable, Codable {
+    var id = UUID()
     let name: String
     let type: String
     let amount: Int
 }
 
 class Expenses: ObservableObject {
-    @Published var items = [ExpenseItem]()
+    @Published var items = [ExpenseItem]() {
+        didSet{
+            let encoder = JSONEncoder()
+            if let encoded = try? encoder.encode(items) {
+                UserDefaults.standard.set(encoded, forKey: "Items")
+            }
+        }
+    }
+
+    init() {
+        if let items = UserDefaults.standard.data(forKey: "Items") {
+            let decoder = JSONDecoder()
+            // note the use self at `[ExpenseItem].self` as type object
+            if let decoded = try? decoder.decode([ExpenseItem].self, from: items) {
+                self.items = decoded
+                return
+            }
+        }
+        self.items = []
+    }
 }
 
 struct ContentView: View {
     @ObservedObject var expenses = Expenses()
+    @State private var showingAddExpense = false
 
     var body: some View {
         NavigationView {
             List {
-                ForEach(expenses.items, id: \.name) { item in
-                    Text(item.name)
+                ForEach(expenses.items) { item in
+                    HStack {
+                        VStack(alignment: .leading) {
+                            Text(item.name)
+                                .font(.headline)
+                            Text(item.type)
+                        }
+                        Spacer()
+                        Text("$\(item.amount)")
+                    }
+
                 }
                 .onDelete(perform: removeItems)
             }
             .navigationTitle("iExpense")
             .navigationBarItems(trailing:
                 Button(action: {
-                    let expense = ExpenseItem(name: "Test", type: "A", amount: 10)
-                    expenses.items.append(expense)
+                    showingAddExpense = true
                 }) {
                     Image(systemName: "plus")
                 }
             )
+            .sheet(isPresented: $showingAddExpense) {
+                AddView(expense: expenses)
+            }
         }
     }
 
